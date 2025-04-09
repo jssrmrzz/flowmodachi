a14e864538d8a5a8a8aef8f6ba758c70607079ce
JessR <jssrmrz08@gmail.com>
2025-04-09 14:43:59 -0500

    Initial Commit

---
+++ Flowmodachi/FlowmodachiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowmodachiApp.swift
+//  Flowmodachi
+//
+//  Created by Jess Ramirez on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct FlowmodachiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlowmodachiTests/FlowmodachiTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlowmodachiTests.swift
+//  FlowmodachiTests
+//
+//  Created by Jess Ramirez on 4/9/25.
+//
+
+import Testing
+
+struct FlowmodachiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
